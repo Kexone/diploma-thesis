e9f29c025bf1cebe702989945c09a8028c3a956e
Kexone <kami.extreme@gmail.com>
2017-11-21 22:07:21 +0100

    svm testing by differential evolution

--- pedestrian/source/test/svmTest.cpp
+++ pedestrian/source/test/svmTest.cpp
@@ -24,19 +24,42 @@ SvmTest::SvmTest()
 void SvmTest::runSvmTest()
 {
 	std::string samplesPath = "samples/";
-	std::string posSamples = samplesPath + "listPosMin.txt";
-	std::string negSamples = samplesPath + "listNegMin.txt";
-	std::string posTest = samplesPath + "listPosTestMin.txt";
-	std::string negTest = samplesPath + "listNegTestMin.txt";
-	std::string classifierName = "test.yml";
+	posSamples = samplesPath + "listPosMin.txt";
+	negSamples = samplesPath + "listNegMin.txt";
+	posTest = samplesPath + "listPosTestMin.txt";
+	negTest = samplesPath + "listNegTestMin.txt";
+	classifierName = "test.yml";
+	actualIter = 1;
 
-	std::vector< cv::Mat > posTestLst;
-	std::vector< cv::Mat > negTestLst;
-	int actualIter = 1;
+	this->maxIterations = 300;
+	this->termCriteria = CV_TERMCRIT_ITER + CV_TERMCRIT_EPS;
+	this->kernel = cv::ml::SVM::LINEAR;
+	this->type = cv::ml::SVM::NU_SVC;
+	this->epsilon = 1.e-6;
+	this->coef0 = 0.0;
+	this->degree = 3;
+	this->gamma = 0.1;
+	this->nu = 0.1;
+	this->p = 0.1;
+	this->c = 0.1;
 
 	std::cout << "\n\n******************" << std::endl;
 	std::cout << "***  SVM TEST  ***" << std::endl;
 	std::cout << "******************" << std::endl;
+	std::cout << "DIFFERENTIAL EVOLUTION (1)\RANDOM TESTING (2)\nCHOOSE TYPE: ";
+	std::cin >> typeTest;
+	if(typeTest == 1)
+	{
+		loadMats(posTest, posTestLst);
+		loadMats(negTest, negTestLst);
+		initResultFile();
+		SvmTest dims(3);
+		// Create Differential Evolution optimizer with population size of 50
+		de::DifferentialEvolution de(dims, 50);
+
+		// Optimize for 200 iterations
+		de.Optimize(1000, true);
+	}
 	std::cout << "NUMBER OF REPEATS: ";
 	std::cin >> maxIterTest;
 
@@ -45,7 +68,7 @@ void SvmTest::runSvmTest()
 		std::cout << "NUMBER OF ITERATIONS MUST BE GREATER THAN ZERO!\nEND" << std::endl;
 		return;
 	}
-	std::cout << "SOFT (1) OR GROSS (2) REGRESSION TEST: ";
+	std::cout << "SOFT (1) OR GROSS (2) ITERATION: ";
 	std::cin >> typeTest;
 
 	if(typeTest == 1)
@@ -76,49 +99,10 @@ void SvmTest::runSvmTest()
 
 	std::cout << std::endl  << "*** TESTING HAS STARTED ***" << std::endl << std::endl;
 
-	
-	this->maxIterations = 300;
-	this->termCriteria = CV_TERMCRIT_ITER + CV_TERMCRIT_EPS;
-	this->kernel = cv::ml::SVM::LINEAR;
-	this->type = cv::ml::SVM::NU_SVC;
-	this->epsilon = 1.e-6;
-	this->coef0 = 0.0;
-	this->degree = 3;
-	this->gamma = 0.1;
-	this->nu = 0.1;
-	this->p = 0.1;
-	this->c = 0.1;
-
 	while(actualIter != maxIterTest+1)
 	{
-		nTruePos = 0;
-		nFalsePos = 0;
-		nTrueNeg = 0;
-		nFalseNeg = 0;
+		process();
 
-		std::cout << std::endl << actualIter << ". ITERATION OF TESTING" << std::endl;
-
-		TrainHog th = TrainHog(maxIterations, termCriteria, kernel, type, epsilon, coef0, degree, gamma, nu, p, c, classifierName);
-
-		trainTime = clock();
-		th.fillVectors(posSamples);
-		th.fillVectors(negSamples, true);
-		th.train(false);
-		trainTime = clock() - trainTime;
-
-		Hog h = Hog(classifierName);
-
-		std::cout << " << TESTING SVM >>" << std::endl;
-
-		classTime = clock();
-		h.detect(posTestLst, nTruePos, nFalsePos);
-		h.detect(negTestLst, nTrueNeg, nFalseNeg, false);
-		classTime = clock() - classTime;
-
-		std::cout << "POS DETECTION [T/F] " << nTruePos << "/" << nFalsePos << std::endl;
-		std::cout << "NEG DETECTION [T/F] " << nTrueNeg << "/" << nFalseNeg << std::endl;
-
-		print2File(actualIter);
 		if(typeIncr == 1)	incrementValues();
 		else if(typeIncr == 2)
 		{
@@ -170,6 +154,40 @@ void SvmTest::initResultFile()
 	file << "__________________________________" << std::endl;
 
 	file.close();
+}
+
+void SvmTest::process()
+{
+	nTruePos = 0;
+	nFalsePos = 0;
+	nTrueNeg = 0;
+	nFalseNeg = 0;
+
+	std::cout << std::endl << actualIter << ". ITERATION OF TESTING" << std::endl;
+
+	TrainHog th = TrainHog(maxIterations, termCriteria, kernel, type, epsilon, coef0, degree, gamma, nu, p, c, classifierName);
+
+	trainTime = clock();
+	th.fillVectors(posSamples);
+	th.fillVectors(negSamples, true);
+	th.train(false);
+	trainTime = clock() - trainTime;
+
+	Hog h = Hog(classifierName);
+
+	std::cout << " << TESTING SVM >>" << std::endl;
+
+	classTime = clock();
+	h.detect(posTestLst, nTruePos, nFalsePos);
+	h.detect(negTestLst, nTrueNeg, nFalseNeg, false);
+	classTime = clock() - classTime;
+
+	std::cout << "POS DETECTION [T/F] " << nTruePos << "/" << nFalsePos << std::endl;
+	std::cout << "NEG DETECTION [T/F] " << nTrueNeg << "/" << nFalseNeg << std::endl;
+
+	print2File(actualIter);
+
+	accuracy = (nTruePos + nTrueNeg ) / (nTruePos + nTrueNeg + nFalsePos + nFalseNeg) ;
 }
 
 void SvmTest::print2File(int actualIter)
@@ -224,4 +242,32 @@ void SvmTest::incrementValues()
 	this->nu += parChange;
 	this->p += parChange;
 	this->c += parChange;
+}
+
+
+double SvmTest::EvaluteCost(std::vector<double> inputs) const
+{
+	assert(inputs.size() == 3);
+
+	const_cast<SvmTest*>(this)->c = inputs[0];
+	const_cast<SvmTest*>(this)->p = inputs[1];
+	const_cast<SvmTest*>(this)->nu = inputs[2];
+	const_cast<SvmTest*>(this)->process();
+
+	return const_cast<SvmTest*>(this)->accuracy;
+}
+
+unsigned int SvmTest::NumberOfParameters() const
+{
+	return 3;
+}
+
+std::vector<de::IOptimizable::Constraints> SvmTest::GetConstraints() const
+{
+	std::vector<Constraints> constr(NumberOfParameters());
+	for (auto& c : constr)
+	{
+		c = Constraints(0.0, 1.0, true);
+	}
+	return constr;
 }
--- pedestrian/source/test/svmTest.h
+++ pedestrian/source/test/svmTest.h
@@ -1,13 +1,18 @@
 #ifndef SVMTEST_H
 #define SVMTEST_H
+#include "../../3dparty/de/DifferentialEvolution.h"
 
-class SvmTest
+class SvmTest : public de::IOptimizable
 {
 private:
 	void incrementValues();
 	void print2File(int actualIter);
 	void loadMats(std::string &samplesPath, std::vector< cv::Mat > &samples);
 	void initResultFile();
+	void process();
+	double EvaluteCost(std::vector<double> inputs) const override;
+	unsigned int NumberOfParameters() const override;	
+	std::vector<Constraints> GetConstraints() const override;
 
 	int maxIterations;
 	int termCriteria;
@@ -24,6 +29,17 @@ private:
 	clock_t trainTime;
 	clock_t classTime;
 
+	std::string classifierName;
+	std::string posSamples;
+	std::string negSamples;
+	std::string posTest;
+	std::string negTest;
+	std::vector< cv::Mat > posTestLst;
+	std::vector< cv::Mat > negTestLst;
+	int actualIter;
+
+	float accuracy;
+
 	int nTruePos;
 	int nFalsePos;
 	int nTrueNeg;
@@ -35,8 +51,14 @@ private:
 	int typeTest;
 	int typeIncr;
 
+	unsigned int m_dim;
+
 public:
 	SvmTest();
+	SvmTest(unsigned int dims)
+	{
+		m_dim = dims;
+	}
 	void runSvmTest();
 };
 
