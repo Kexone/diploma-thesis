330da0a9b7e2465b12699f5e894f31840b8b38db
Kexone <kami.extreme@gmail.com>
2017-12-06 01:53:38 +0100

    combined training

---
+++ pedestrian/source/train/combinedTrainHog.h
@@ -0,0 +1,21 @@
+﻿#ifndef COMBINEDTRAINHOG_H
+#define COMBINEDTRAINHOG_H
+#include <string>
+#include "trainhog.h"
+#include "trainfhog.h"
+
+/**
+ * This class combining calculated features from OpenCV HOG to Dlib SVM classification training
+ * @TODO train Dlib SVM from OpenCV HOG features
+ */
+
+class CombinedTrainHog : public TrainHog, TrainFHog
+{
+
+public:
+	void train(std::string posSamplesPath, std::string negSamplesPath);
+	
+};
+
+
+#endif // COMBINEDTRAINHOG_H
