1a4b7697342b3d2d52d383430cd1cb4cd339c475
Kexone <kami.extreme@gmail.com>
2018-01-21 21:03:37 +0100

    training cascade classifier - testing

---
+++ pedestrian/source/train/trainCascade.cpp
@@ -0,0 +1,34 @@
+#include "trainCascade.h"
+
+//@TODO make choosable params
+TrainCascade::TrainCascade(std::string classPath, std::string posSamples, std::string negSamples)
+{
+	//_classifierPath = classPath;
+	//_posSamples = posSamples;
+	//_negSamples = negSamples;
+	//_stageParams.printAttrs();
+}
+
+void TrainCascade::train()
+{
+	//const cv::Ptr<CvFeatureParams> _featureParams[3] = { cv::makePtr<CvHaarClassifier>(), cv::makePtr<CvHOGFeatureParams>(), cv::makePtr<CvLBPFeatureParams>() };
+
+	//cv::setNumThreads(_numThreads );
+
+	//_classifier.train(
+	//		_classifierPath, 
+	//		_posSamples, 
+	//		_negSamples, 
+	//		_numPos, 
+	//		_numNeg, 
+	//		_precalcValBufSize, 
+	//		_precalcIdxBufSize, 
+	//		_numStages, 
+	//		_cascadeParams, 
+	//		*_featureParams[_cascadeParams.featureType], 
+	//		_stageParams, 
+	//		_baseFormatSave, 
+	//		_acceptanceRatioBreakValue
+	//	);
+	
+}
